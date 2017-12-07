fded5680fb8500e39dc207dcae61abb174e7838d
John-Nathan Hill <john.nat.hill@gmail.com>
2017-12-08 00:07:58 +0100

    init

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@
 
 	//Init Firebase
 	const config = {
-	    apiKey: "AIzaSyCa2WkqUPFA75dVje_Xqv0EVW4bHJaQoh0",
-	    authDomain: "iot-project-1e8b6.firebaseapp.com",
-	    databaseURL: "https://iot-project-1e8b6.firebaseio.com/",
-	    storageBucket: "gs://iot-project-1e8b6.appspot.com/",
+		apiKey: "AIzaSyDGDb7UPuSQ6ZwVrI0zBnSj2tluCqLtbwM",
+		authDomain: "pickmeup-b6abc.firebaseapp.com",
+		databaseURL: "https://pickmeup-b6abc.firebaseio.com",
+		projectId: "pickmeup-b6abc",
+		storageBucket: "pickmeup-b6abc.appspot.com",
+		messagingSenderId: "787993144427"
+
 	};
 	firebase.initializeApp(config);
 
