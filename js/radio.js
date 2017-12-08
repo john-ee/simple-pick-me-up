551f58126012d7866cf768560e32a768ea14ae50
Nina TANG <niina_live@hotmail.com>
2017-12-08 04:50:37 +0100

    Radio working perfectly, still need to modify the css

--- js/radio.js
+++ js/radio.js
@@ -1,44 +1,29 @@
- var radio = require('radio.json');
+
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", 'https://www.radioking.com/widgets/currenttrack.php?radio=116593&format=json', false);
+xhReq.send(null);
+var jsonObject = JSON.parse(xhReq.responseText);
 
 
-function testFunction() {
-	alert("Hello world!");
-}
+// console.log(jsonObject);
+// console.log(jsonObject.artist);
+// console.log(jsonObject.title);
+// console.log(jsonObject.album);
+// console.log(jsonObject.cover);
+// console.log(jsonObject.buy_link);
+// console.log(jsonObject.started_at);
+// console.log(jsonObject.end_at);
+// console.log(jsonObject.duration);	
+
+document.getElementById("artist_name").innerHTML = jsonObject.artist;
+document.getElementById("son_title").innerHTML = jsonObject.title;
+document.getElementById("album_name").innerHTML = jsonObject.album;
+document.getElementById("cover_link").innerHTML = jsonObject.cover;
+document.getElementById("buy").innerHTML = jsonObject.buy_link;
+document.getElementById("start").innerHTML = jsonObject.started_at;
+document.getElementById("end").innerHTML = jsonObject.end_at;
+document.getElementById("son_duration").innerHTML = jsonObject.duration;
+
+document.getElementById("cover_img").src = jsonObject.cover;
 
 
-var myData = JSON.parse(radio);
-alert(myData[0].artist);
-alert(myData[0].title);
-alert(myData[0].album);
-alert(myData[0].cover);
-alert(myData[0].buy_link);
-alert(myData[0].started_at);
-alert(myData[0].end_at);
-alert(myData[0].duration);	
-
-
-// var data = $.getJSON('https://www.radioking.com/widgets/currenttrack.php?radio=116593&format=json', function(data) {
-//     //data is the JSON string
-//     console.log("success");
-// });
-
-// function getJSONP(url, success) {
-
-//     var ud = '_' + +new Date,
-//         script = document.createElement('script'),
-//         head = document.getElementsByTagName('head')[0] 
-//                || document.documentElement;
-
-//     window[ud] = function(data) {
-//         head.removeChild(script);
-//         success && success(data);
-//     };
-
-//     script.src = url.replace('callback=?', 'callback=' + ud);
-//     head.appendChild(script);
-
-// }
-
-// getJSONP('https://www.radioking.com/widgets/currenttrack.php?radio=116593&format=json', function(data){
-//     console.log(data);
-// });  
